7f0e9a9e94fa9cd5b1b01c6085879c0d3ab1cf21
Linas M <lincasm@gmail.com>
2025-06-04 23:39:06 +0300

    Fix Many FIX

--- vendure-showcase/src/vendure-config.ts
+++ vendure-showcase/src/vendure-config.ts
@@ -1,19 +1,9 @@
-import {
-    dummyPaymentHandler,
-    DefaultJobQueuePlugin,
-    DefaultSchedulerPlugin,
-    DefaultSearchPlugin,
-    VendureConfig,
-} from '@vendure/core';
-import { defaultEmailHandlers, EmailPlugin, FileBasedTemplateLoader } from '@vendure/email-plugin';
-import { AssetServerPlugin } from '@vendure/asset-server-plugin';
-import { AdminUiPlugin } from '@vendure/admin-ui-plugin';
-import { GraphiqlPlugin } from '@vendure/graphiql-plugin';
+import { VendureConfig } from '@vendure/core';
 import 'dotenv/config';
 import path from 'path';
 
-const IS_DEV = process.env.APP_ENV === 'dev';
-const serverPort = +process.env.PORT || 3000;
+const IS_DEV = (process.env.APP_ENV ?? 'prod') === 'dev';
+const serverPort = +(process.env.PORT ?? '3000');
 
 export const config: VendureConfig = {
     apiOptions: {
@@ -32,8 +22,8 @@ export const config: VendureConfig = {
     authOptions: {
         tokenMethod: ['bearer', 'cookie'],
         superadminCredentials: {
-            identifier: process.env.SUPERADMIN_USERNAME,
-            password: process.env.SUPERADMIN_PASSWORD,
+            identifier: process.env.SUPERADMIN_USERNAME ?? 'superadmin',
+            password: process.env.SUPERADMIN_PASSWORD ?? 'superadmin',
         },
         cookieOptions: {
           secret: process.env.COOKIE_SECRET,
@@ -43,7 +33,7 @@ export const config: VendureConfig = {
         type: 'postgres',
         // See the README.md "Migrations" section for an explanation of
         // the `synchronize` and `migrations` options.
-        synchronize: process.env.synchronize,
+        synchronize: process.env.synchronize === 'true',
         migrations: [path.join(__dirname, './migrations/*.+(js|ts)')],
         logging: false,
         database: process.env.DB_NAME,
@@ -53,50 +43,14 @@ export const config: VendureConfig = {
             rejectUnauthorized: false,
         } : undefined,
         host: process.env.DB_HOST,
-        port: +process.env.DB_PORT,
+        port: +(process.env.DB_PORT ?? '5432'),
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
-    },
-    paymentOptions: {
-        paymentMethodHandlers: [dummyPaymentHandler],
     },
     // When adding or altering custom field definitions, the database will
     // need to be updated. See the "Migrations" section in README.md.
     customFields: {},
-    plugins: [
-        GraphiqlPlugin.init(),
-        AssetServerPlugin.init({
-            route: 'assets',
-            assetUploadDir: path.join(__dirname, '../static/assets'),
-            // For local dev, the correct value for assetUrlPrefix should
-            // be guessed correctly, but for production it will usually need
-            // to be set manually to match your production url.
-            assetUrlPrefix: IS_DEV ? undefined : 'https://www.my-shop.com/assets/',
-        }),
-        DefaultSchedulerPlugin.init(),
-        DefaultJobQueuePlugin.init({ useDatabaseForBuffer: true }),
-        DefaultSearchPlugin.init({ bufferUpdates: false, indexStockStatus: true }),
-        EmailPlugin.init({
-            devMode: true,
-            outputPath: path.join(__dirname, '../static/email/test-emails'),
-            route: 'mailbox',
-            handlers: defaultEmailHandlers,
-            templateLoader: new FileBasedTemplateLoader(path.join(__dirname, '../static/email/templates')),
-            globalTemplateVars: {
-                // The following variables will change depending on your storefront implementation.
-                // Here we are assuming a storefront running at http://localhost:8080.
-                fromAddress: '"example" <noreply@example.com>',
-                verifyEmailAddressUrl: 'http://localhost:8080/verify',
-                passwordResetUrl: 'http://localhost:8080/password-reset',
-                changeEmailAddressUrl: 'http://localhost:8080/verify-email-address-change'
-            },
-        }),
-        AdminUiPlugin.init({
-            route: 'admin',
-            port: serverPort + 2,
-            adminUiConfig: {
-                apiPort: serverPort,
-            },
-        }),
-    ],
+    paymentOptions: {
+        paymentMethodHandlers: [],
+    },
 };
